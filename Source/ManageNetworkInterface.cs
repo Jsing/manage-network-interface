c45117a989d703b28bea5c2b90d7149ea2016588
Jsing <joosing711@gmail.com>
2018-01-18 14:02:50 +0900

    Configure network interface properties
    - Both static and dynamic are configurable.

---
+++ Source/ManageNetworkInterface.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Net.NetworkInformation;
+
+
+public static class NetworkInterfaceManangement
+{
+
+    public static void EnableStatic(string networkAdapterName, string[] ipAddresses, string[] subnetMasks, string[] gateways, string[] dnses)
+    {
+        string macAddress = _NameToMacAddress(networkAdapterName);
+
+        ManagementClass netAdapterConfigClass = new ManagementClass("Win32_NetworkAdapterConfiguration");
+        ManagementObjectCollection netAdapterConfigObjectCollect = netAdapterConfigClass.GetInstances();
+
+        foreach (ManagementObject netAdapterConfigObject in netAdapterConfigObjectCollect)
+        {
+            if (netAdapterConfigObject["MACAddress"] != null && netAdapterConfigObject["MACAddress"].Equals(macAddress))
+            {
+                // IPs, Subnet Masks
+                ManagementBaseObject EnableStaticFunc = netAdapterConfigObject.GetMethodParameters("EnableStatic");
+
+                EnableStaticFunc["IPAddress"] = ipAddresses;
+                EnableStaticFunc["SubnetMask"] = subnetMasks;
+
+                netAdapterConfigObject.InvokeMethod("EnableStatic", EnableStaticFunc, null);
+
+                // Gateways 
+                ManagementBaseObject SetGatewaysFunc = netAdapterConfigObject.GetMethodParameters("SetGateways");
+
+                SetGatewaysFunc["DefaultIPGateway"] = gateways;
+
+                netAdapterConfigObject.InvokeMethod("SetGateways", SetGatewaysFunc, null);
+
+                // DNS 
+                ManagementBaseObject EnableDNSFunc = netAdapterConfigObject.GetMethodParameters("SetDNSServerSearchOrder");
+
+                EnableDNSFunc["DNSServerSearchOrder"] = dnses;
+
+                netAdapterConfigObject.InvokeMethod("SetDNSServerSearchOrder", EnableDNSFunc, null);
+
+            }
+        }
+    }
+
+    public static void EnableDynamic(string networkAdapterName)
+    {
+        string macAddress = _NameToMacAddress(networkAdapterName);
+
+        ManagementClass netAdapterConfigClass = new ManagementClass("Win32_NetworkAdapterConfiguration");
+        ManagementObjectCollection netAdapterConfigObjectCollect = netAdapterConfigClass.GetInstances();
+
+        foreach (ManagementObject netAdapterConfigObject in netAdapterConfigObjectCollect)
+        {
+            if (netAdapterConfigObject["MACAddress"] != null && netAdapterConfigObject["MACAddress"].Equals(macAddress))
+            {
+                // DHCP 
+                ManagementBaseObject EnableDynamicFunc = netAdapterConfigObject.GetMethodParameters("EnableDHCP");
+
+                netAdapterConfigObject.InvokeMethod("EnableDHCP", EnableDynamicFunc, null);
+
+                // Dynamic DNS 
+                ManagementBaseObject EnableDNSFunc = netAdapterConfigObject.GetMethodParameters("SetDNSServerSearchOrder");
+
+                EnableDNSFunc["DNSServerSearchOrder"] = null;
+
+                netAdapterConfigObject.InvokeMethod("SetDNSServerSearchOrder", EnableDNSFunc, null);
+            }
+
+        }
+    }
+
+    private static string _NameToMacAddress(string networkAdapterName)
+    {
+        string macAddress = string.Empty;
+        ManagementClass netAdapterClass = new ManagementClass("Win32_NetworkAdapter");
+        ManagementObjectCollection netAdapterObjectSet = netAdapterClass.GetInstances();
+
+        foreach (ManagementObject netAdapterObj in netAdapterObjectSet)
+        {
+            if (netAdapterObj["NetConnectionID"] != null && netAdapterObj["NetConnectionID"].Equals(networkAdapterName))
+            {
+                macAddress = (string)netAdapterObj["MACAddress"];
+            }
+        }
+
+        if (macAddress == string.Empty)
+        {
+            throw new ApplicationException(string.Format("\"{0}\" Network Adapter is not available."));
+        }
+
+        return macAddress;
+    }
+
+
+}
---
+++ Source/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string networkAdapterName = "이더넷";
+        string[] ipList = new string[] { "192.168.1.3"};
+        string[] netMaskList = new string[] { "255.255.255.0" };
+        string[] gatewayList = new string[] { "192.168.2.1"}; 
+        string[] dnsList = new string[] {"192.168.1.111"};
+
+        NetworkInterfaceManangement.EnableStatic(networkAdapterName, ipList, netMaskList, gatewayList, dnsList);
+        
+        //NetworkInterfaceManangement.EnableDynamic(nicName);
+
+        Console.WriteLine("Network Interface configuration complete!");
+    }
+}
